6b4e3da53d27b822ea651d48414b2bb99307540d
Daniel McCullough <kisa7081@gmail.com>
2019-02-18 13:37:38 -0600

    First FooBooks0 commit.  More to do.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require 'helpers.php';
+require 'logic.php';
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+
+    <title>Foobooks0</title>
+    <meta charset='utf-8'/>
+
+</head>
+
+<body>
+
+    <h1>Fookbooks0</h1>
+
+</body>
+
+</html>
